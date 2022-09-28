d5f46ad9a785819d0522d76a6cb58aba73124aa8
thanhtaita <102627561+thanhtaita@users.noreply.github.com>
2022-09-28 17:13:24 -0500

    Update sketch.js
    change the colors

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 
 let clrs = [
-'#E27F34', //top 0
-'#FEF254', //middle top 1
-'#A36C33', //middle strip 2
-'#042060', //middle bottom 3
-'#3778AE' //bottom 4
+'green', //top 0
+'black', //middle top 1
+'blue', //middle strip 2
+'red', //middle bottom 3
+'pink' //bottom 4
 ]
 
 
